ebffc1ff592bf38de1c3b657577314741fbff2e1
AnnaSimashka <81093163+AnnaSimashka@users.noreply.github.com>
2022-03-30 00:07:32 +0400

    Установлен eslint
    Исправление недочетов по оформлению кода.

--- board.js
+++ board.js
@@ -1,116 +1,115 @@
 import Cell from './cell.js';
 
 export default class Board {
-    constructor(widthBoard, squares, wrapper) {
-        this.widthBoard = 4;
-        this.squares = [];
-        this.wrapper = document.querySelector('.grid');
+  constructor(widthBoard, squares, wrapper) {
+    this.widthBoard = 4;
+    this.squares = [];
+    this.wrapper = document.querySelector('.grid');
+  }
+
+  init() {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < this.widthBoard * this.widthBoard; i++) {
+      const cell = new Cell();
+
+      fragment.appendChild(cell.getNewElement());
+      this.squares.push(cell);
     }
 
-    init() {
-        const fragment = document.createDocumentFragment();
+    this.wrapper.appendChild(fragment);
+  }
 
-        for (let i = 0; i < this.widthBoard * this.widthBoard; i++) {
-            const cell = new Cell();
+  generateNewCell() {
+    const randomNumber = Math.floor(Math.random() * this.squares.length);
 
-            fragment.appendChild(cell.getNewElement());
-            this.squares.push(cell);
-        }
+    if (this.squares[randomNumber].getValue() === '') {
+      this.squares[randomNumber].setValue(2);
+      // проверить на GameOver
+    } else {
+      this.generateNewCell();
+    }
+  }
 
-        this.wrapper.appendChild(fragment);
+  movingColumn(direction) {
+    for (let i = 0; i < this.widthBoard; i++) {
+      this.fillColumn(i, direction === 'up');
+    }
+  }
+
+  // объединение значений в столбцах
+  fillColumn(indexColumn, isUp) {
+    const column = [];
+
+    for (let i = 0; i < this.widthBoard; i++) {
+      column.push(this.squares[indexColumn + this.widthBoard * i].getValue());
     }
 
-    generateNewCell() {
-        const randomNumber = Math.floor(Math.random() * this.squares.length);
+    const filteredColumn = column.filter((num) => num);
+    const emptyCellInColumnSize = this.widthBoard - filteredColumn.length;
 
-        if (this.squares[randomNumber].getValue() === '') {
-            this.squares[randomNumber].setValue(2);
-            // проверить на GameOver
-        } else {
-            this.generateNewCell();
-        }
+    const newColumn = this.makeNewSequence(filteredColumn, emptyCellInColumnSize, isUp);
+
+    newColumn.forEach((value, i) => {
+      this.squares[indexColumn + (this.widthBoard * i)].setValue(value);
+    });
+  }
+
+  makeNewSequence(numbers, emptySequensSize, isReverse) {
+    const emptySequence = Array(emptySequensSize).fill('');
+
+    return isReverse ? numbers.concat(emptySequence) : emptySequence.concat(numbers);
+  }
+
+  // суммирование значений
+  combineColumn() {
+    for (let i = 15; i >= this.widthBoard; i--) {
+      if ((this.squares[i].getValue() === this.squares[i - this.widthBoard].getValue()) && this.squares[i].getValue() !== '') {
+        const combinedTotal = parseInt(this.squares[i].getValue()) + parseInt(this.squares[i - this.widthBoard].getValue());
+
+        this.squares[i].setValue(combinedTotal);
+        this.squares[i - this.widthBoard].setValue('');
+      }
+    }
+    // проверить на выигрыш
+  }
+
+  movingRow(direction) {
+    for (let i = 0; i < this.widthBoard * this.widthBoard; i++) {
+      if (i % this.widthBoard === 0) {
+        this.fillRow(i, direction === 'left');
+      }
+    }
+  }
+
+  // объединение значений в строках
+  fillRow(rowIndex, isLeft) {
+    const row = [];
+
+    for (let i = 0; i < this.widthBoard; i++) {
+      row.push(this.squares[rowIndex + i].getValue());
     }
 
-    movingColumn(direction) {
-        for (let i = 0; i < this.widthBoard; i++) {
-            this.fillColumn(i, direction === 'up');
-        }
+    const filteredRow = row.filter((num) => num);
+    const emptyCellInRowSize = this.widthBoard - filteredRow.length;
+
+    const newRow = this.makeNewSequence(filteredRow, emptyCellInRowSize, isLeft);
+
+    newRow.forEach((value, i) => {
+      this.squares[rowIndex + i].setValue(value);
+    });
+  }
+
+  combineRow() {
+    for (let i = 15; i > 0; i--) {
+      if ((this.squares[i].getValue() === this.squares[i - 1].getValue()) && this.squares[i].getValue() !== '' && i % this.widthBoard !== 0) {
+        const combinedTotal = parseInt(this.squares[i].getValue()) + parseInt(this.squares[i - 1].getValue())
+
+        this.squares[i].setValue(combinedTotal);
+        this.squares[i - 1].setValue('');
+      }
     }
 
-    // объединение значений в столбцах
-    fillColumn(indexColumn, isUp) {
-        const column = [];
-
-        for (let i = 0; i < this.widthBoard; i++) {
-            column.push(this.squares[indexColumn + this.widthBoard * i].getValue());
-        }
-
-        let filteredColumn = column.filter(num => num);
-        let emptyCellInColumnSize = this.widthBoard - filteredColumn.length;
-
-        let newColumn = this.makeNewSequence(filteredColumn, emptyCellInColumnSize, isUp);
-
-        newColumn.forEach((value, i) => {
-            this.squares[indexColumn + (this.widthBoard * i)].setValue(value);
-        });
-    }
-
-    makeNewSequence(numbers, emptySequensSize, isReverse) {
-        let emptySequence = Array(emptySequensSize).fill('');
-
-        return isReverse ? numbers.concat(emptySequence) : emptySequence.concat(numbers);
-    }
-
-    //суммирование значений
-    combineColumn() {
-        for (let i = 15; i >= 4; i--) {
-            if ((this.squares[i].getValue() === this.squares[i - this.widthBoard].getValue()) && this.squares[i].getValue() !== '') {
-                let combinedTotal = parseInt(this.squares[i].getValue()) + parseInt(this.squares[i - this.widthBoard].getValue());
-
-                this.squares[i].setValue(combinedTotal);
-                this.squares[i - this.widthBoard].setValue('');
-            }
-        }
-        // проверить на выигрыш
-    }
-
-    movingRow(direction) {
-        for (let i = 0; i < this.widthBoard * this.widthBoard; i++) {
-            if (i % 4 === 0) {
-                this.fillRow(i, direction === 'left');
-            }
-        }
-    }
-
-    // объединение значений в строках
-    fillRow(rowIndex, isLeft) {
-        const row = [];
-
-        for (let i = 0; i < this.widthBoard; i++) {
-            row.push(this.squares[rowIndex + i].getValue());
-        }
-
-        let filteredRow = row.filter(num => num);
-        let emptyCellInRowSize = this.widthBoard - filteredRow.length;
-
-        let newRow = this.makeNewSequence(filteredRow, emptyCellInRowSize, isLeft);
-
-        newRow.forEach((value, i) => {
-            this.squares[rowIndex + i].setValue(value);
-        });
-    }
-
-    combineRow() {
-        for (let i = 15; i > 0; i--) {
-            if ((this.squares[i].getValue() === this.squares[i - 1].getValue()) && this.squares[i].getValue() !== '' && i % 4 !== 0) {
-                let combinedTotal = parseInt(this.squares[i].getValue()) + parseInt(this.squares[i - 1].getValue())
-
-                this.squares[i].setValue(combinedTotal);
-                this.squares[i - 1].setValue('');
-            }
-        }
-
-        // проверить на выигрыш
-    }
-
+    // проверить на выигрыш
+  }
 }
--- script.js
+++ script.js
@@ -7,49 +7,48 @@ const scoreDisplay = document.querySelector('.score');
 const resultDisplay = document.querySelector('.result');
 
 class GameManager {
-    constructor(isGameOver, score, board) {
-        this.isGameOver = false;
-        this.score = 0;
-        this.board = null;
-    }
+  constructor(isGameOver, score, board) {
+    this.isGameOver = false;
+    this.score = 0;
+    this.board = null;
+  }
 
-    init() {
-        this.board = new Board();
-        this.board.init();
-        this.board.generateNewCell();
-        this.board.generateNewCell();
-        document.addEventListener('keyup', this.clickControl.bind(this));
-    }
+  init() {
+    this.board = new Board();
+    this.board.init();
+    this.board.generateNewCell();
+    this.board.generateNewCell();
+    document.addEventListener('keyup', this.clickControl.bind(this));
+  }
 
-    checkIsGameOver() {
-        console.log('checkIsGameOver')
-    }
+  checkIsGameOver() {
+    console.log('checkIsGameOver');
+  }
 
-    clickControl(event) {
-        if (event.key === 'ArrowUp') {
-            this.board.movingColumn('up');
-            this.board.combineColumn();
-            this.board.movingColumn('up');
-            this.board.generateNewCell();
-        } else if (event.key === 'ArrowDown') {
-            this.board.movingColumn();
-            this.board.combineColumn();
-            this.board.movingColumn();
-            this.board.generateNewCell();
-        } else if (event.key === 'ArrowLeft') {
-            this.board.movingRow('left');
-            this.board.combineRow();
-            this.board.movingRow('left');
-            this.board.generateNewCell();
-        } else if (event.key === 'ArrowRight') {
-            this.board.movingRow();
-            this.board.combineRow();
-            this.board.movingRow();
-            this.board.generateNewCell();
-        }
+  clickControl(event) {
+    if (event.key === 'ArrowUp') {
+      this.board.movingColumn('up');
+      this.board.combineColumn();
+      this.board.movingColumn('up');
+      this.board.generateNewCell();
+    } else if (event.key === 'ArrowDown') {
+      this.board.movingColumn();
+      this.board.combineColumn();
+      this.board.movingColumn();
+      this.board.generateNewCell();
+    } else if (event.key === 'ArrowLeft') {
+      this.board.movingRow('left');
+      this.board.combineRow();
+      this.board.movingRow('left');
+      this.board.generateNewCell();
+    } else if (event.key === 'ArrowRight') {
+      this.board.movingRow();
+      this.board.combineRow();
+      this.board.movingRow();
+      this.board.generateNewCell();
     }
+  }
 }
 
 const start = new GameManager();
 start.init();
-
